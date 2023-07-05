64d73efb6c8ef1f5e2dd63121aad3489af88ed92
sebascrd13 <106779944+sebascrd13@users.noreply.github.com>
2023-07-05 16:01:20 -0600

    Dependencias y creación de BD

---
+++ src/main/resources/BD-Proyecto.sql
@@ -0,0 +1,73 @@
+/*Se crea la base de datos */
+drop schema if exists proyectoFinal;
+drop user if exists usuario_prueba;
+CREATE SCHEMA proyectoFinal ;
+
+use proyectoFinal;
+
+
+/*Se crea un usuario para la base de datos llamado "usuario_prueba" y tiene la contraseña "Usuario_Clave."*/
+create user 'usuario_prueba'@'%' identified by 'Usuar1o_Clave.';
+
+/*Se asignan los prvilegios sobr ela base de datos TechShop al usuario creado */
+grant all privileges on proyectoFinal.* to 'usuario_prueba'@'%';
+flush privileges;
+
+/* la tabla de categoria contiene categorias de productos*/
+create table proyectoFinal.categoria (
+  id_categoria INT NOT NULL AUTO_INCREMENT,
+  descripcion VARCHAR(30) NOT NULL,
+  ruta_imagen varchar(1024),
+  activo bool,
+  PRIMARY KEY (id_categoria))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
+
+create table proyectoFinal.carro (
+  id_carro INT NOT NULL AUTO_INCREMENT,
+  id_categoria INT NOT NULL,
+  marca VARCHAR(50) NOT NULL,
+  modelo VARCHAR(50) NOT NULL,
+  año int,
+  combustible VARCHAR(50) NOT NULL,
+  transmision VARCHAR(50) NOT NULL,
+  clindrada VARCHAR(50) NOT NULL,
+  precio double, 
+  ruta_imagen varchar(1024),
+  activo bool,
+  PRIMARY KEY (id_carro),
+  foreign key fk_carro_categoria (id_categoria) references categoria(id_categoria)  
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
+
+/*Se crea la tabla de usuarios llamada usuario... igual que la clase Usuario*/
+CREATE TABLE proyectoFinal.usuario (
+  id_usuario INT NOT NULL AUTO_INCREMENT,
+  username varchar(20) NOT NULL,
+  password varchar(512) NOT NULL,
+  nombre VARCHAR(20) NOT NULL,
+  apellidos VARCHAR(30) NOT NULL,
+  correo VARCHAR(25) NULL,
+  telefono VARCHAR(15) NULL,
+  ruta_imagen varchar(1024),
+  activo boolean,
+  PRIMARY KEY (id_usuario))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
+
+/*Se crea la tabla de roles de usuarios llamada rol... igual que la clase Rol*/
+create table proyectoFinal.rol (
+  id_rol INT NOT NULL AUTO_INCREMENT,
+  nombre varchar(20),
+  id_usuario int,
+  PRIMARY KEY (id_rol),
+  foreign key fk_rol_usuario (id_usuario) references usuario(id_usuario)
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
+
+
+
+
+
