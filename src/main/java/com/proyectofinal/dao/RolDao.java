79d20820f50aa7934bfa4cda821afb709d9472c3
sebascrd13 <106779944+sebascrd13@users.noreply.github.com>
2023-07-30 13:46:19 -0600

    Clases Java

---
+++ src/main/java/com/proyectofinal/dao/RolDao.java
@@ -0,0 +1,8 @@
+package com.proyectofinal.dao;
+
+import com.proyectofinal.domain.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolDao extends JpaRepository<Rol, Long> {
+
+}
---
+++ src/main/java/com/proyectofinal/dao/UsuarioDao.java
@@ -0,0 +1,14 @@
+package com.proyectofinal.dao;
+
+import com.proyectofinal.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioDao extends JpaRepository<Usuario, Long> {    
+    Usuario findByUsername(String username);
+    
+    Usuario findByUsernameAndPassword(String username, String Password);
+
+    Usuario findByUsernameOrCorreo(String username, String correo);
+
+    boolean existsByUsernameOrCorreo(String username, String correo);
+}
